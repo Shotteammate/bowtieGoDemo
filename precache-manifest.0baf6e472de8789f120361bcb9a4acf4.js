017a0f63114b2cb97097f9e4b7a356d38e481d17
shotteammate <shotteammate@gmail.com>
2020-04-01 15:23:09 +0800

    Updates

---
+++ precache-manifest.0baf6e472de8789f120361bcb9a4acf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e9504cab504410462f98c1a4fe6b76",
+    "url": "/shotteammate.github.io/bowtieGoDemo/index.html"
+  },
+  {
+    "revision": "b2a02b750a3f0a1caf2f",
+    "url": "/shotteammate.github.io/bowtieGoDemo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c561ca5996d6adf91935",
+    "url": "/shotteammate.github.io/bowtieGoDemo/static/js/2.d4cdb6e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shotteammate.github.io/bowtieGoDemo/static/js/2.d4cdb6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a02b750a3f0a1caf2f",
+    "url": "/shotteammate.github.io/bowtieGoDemo/static/js/main.144531db.chunk.js"
+  },
+  {
+    "revision": "5429b13d4e75fc6ff44e",
+    "url": "/shotteammate.github.io/bowtieGoDemo/static/js/runtime-main.e2c74ba1.js"
+  }
+]);
